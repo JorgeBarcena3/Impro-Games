0228ce54fba6306830221a9abc833cd21649f707
Jorge Barcena <jbarcena@simfor.es>
2021-03-22 10:15:25 +0100

    init commit

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyB5hnsMyMo5cbjsOzfCzO95F9LEbAE62Co",
+    authDomain: "impro-games.firebaseapp.com",
+    databaseURL: "https://impro-games-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "impro-games",
+    storageBucket: "impro-games.appspot.com",
+    messagingSenderId: "583276936354",
+    appId: "1:583276936354:web:1ba51cee69c6100f4debfb"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
